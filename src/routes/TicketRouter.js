0a498d13d0f48e8777dd71a8ec8af08bbf63d089
Lucian Jorja <lucianjorjaam@gmail.com>
2023-10-04 20:45:42 -0300

    mispelling

--- src/routes/TicketRouter.js
+++ src/routes/TicketRouter.js
@@ -5,7 +5,7 @@ import { TicketController } from "../controllers/ticketController.js";
 const router = new Router();
 const ticketController = new TicketController();
 
-router.get('/dwadad', ticketController.getTicket );
+router.get('/', ticketController.getTicket );
 router.get('/:cid/tickets', ticketController.getUserTicket);
 router.post('/', checkAuth, ticketController.createTicket);
 router.post('/finalizePurchase', checkAuth, ticketController.finalizePurchase);
